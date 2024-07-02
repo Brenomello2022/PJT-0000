19d1bdf03b6a6b043a0f111f4667ee9968fe3eab
Breno de Mello <110641736+Brenomello2022@users.noreply.github.com>
2024-07-01 23:30:24 -0300

    Arquivo settings

---
+++ jogos-python/tic-tac-toe/projeto-orientado-a-objetos/settings.py
@@ -0,0 +1,38 @@
+# Definições de cores
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Tamanho da janela.
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# Cores das linhas do tabuleiro
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+CYAN = (0, 255, 255)
+ORANGE = (255, 165, 0)
+PINK = (255, 192, 203)
+BROWN = (165, 42, 42)
+DARK_GREEN = (0, 100, 0)
+LIGHT_BLUE = (173, 216, 230)
+GRAY = (128, 128, 128)
+LIGHT_GRAY = (211, 211, 211)
+MAGENTA = (255, 0, 255)
+
+# Cores para os jogadores
+PLAYER_COLORS = {
+    "X": RED,
+    "O": BLUE
+}
+
+# Cores para transição suave
+TRANSITION_COLORS = [RED, GREEN, BLUE, YELLOW, PURPLE, CYAN, ORANGE, PINK, BROWN, DARK_GREEN, LIGHT_BLUE, GRAY, LIGHT_GRAY, MAGENTA]
+
+# Arquivos de áudio
+CLICK_SOUND_FILE = "audios/click_sound.wav"
+BUTTON_SOUND_FILE = "audios/button.wav"
